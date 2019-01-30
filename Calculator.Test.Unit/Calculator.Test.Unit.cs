8e77caa0bbf110ccfd48feac0797b02b3a70e0af
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-01-30 11:42:15 +0100

    Tilføjet Accumulator til divide + start på test af throw i Divide

--- Calculator.Test.Unit/Calculator.Test.Unit.cs
+++ Calculator.Test.Unit/Calculator.Test.Unit.cs
@@ -62,6 +62,12 @@ namespace Calculator.Test.Unit
             Assert.That(uut.Accumulator, Is.EqualTo(result));
         }
 
+        [TestCase(1, 0)]
+        public void divideZeroThrowException(double a, double b)
+        {
+            Assert.That(uut.Divide(a,b), Throws(System.DivideByZeroException));
+        }
+
         [TestCase(1, 0, 0.0)]
         [TestCase(5, 2, 2.5)]
         [TestCase(10, -2, -5.0)]
