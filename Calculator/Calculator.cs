e2292408ef93c2d721863ccc442eda23f945d358
Batmancarlo <43032221+Batmancarlo@users.noreply.github.com>
2019-01-30 11:54:41 +0100

    Multipy, Divide and Power with accumulator add'et

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -26,17 +26,39 @@ namespace Calculator
             return Accumulator;
         }
 
+        public double Multiply(double a)
+        {
+            Accumulator = Accumulator*a ;
+            return Accumulator;
+        }
+
         public double Power(double x, double exp)
         {
             Accumulator = Math.Pow(x, exp);
             return Accumulator;
         }
+
+        public double Power(double exp)
+        {
+            Accumulator = Math.Pow(Accumulator, exp);
+            return Accumulator;
+        }
+
         public double Divide(double dividend, double divisor)
         {
             if ((dividend == 0) || (divisor == 0))
                 return 0;
 
             return dividend / divisor;
+        }
+
+        public double Divide(double divisor)
+        {
+            if ((divisor == 0))
+                return 0;
+
+            Accumulator = (Accumulator / divisor);
+            return Accumulator;
         }
 
         public double Accumulator { get; private set; }
